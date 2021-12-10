1179baf06fecad59f883bb96daa40260e7f8655e
Tejashegde95 <tejashegde.1595>
2021-12-11 01:36:11 +0530

    Changing base url to hosted version

--- src/service/http.service.js
+++ src/service/http.service.js
@@ -1,17 +1,12 @@
-const baseUrl = `https://newsapi.org/v2/top-headlines?`;
+const baseUrl = `http://54.226.18.27:8080/news?`;
 const category = `&category=`;
 const country = `country=`;
 const API_KEY="08e3fdb043fc41f8b43e35ccf3d18b8f";
 export default class HttpService {
 
     getNews(requestParameters, countryCode){
-        return fetch(baseUrl+country+countryCode+category+requestParameters,{
-            headers: {
-                "X-Api-Key": API_KEY,
-            }
-        })
+        return fetch(baseUrl+country+countryCode+category+requestParameters)
         .then(response => response.json());
-
     }
 
     getNewsSearch(query, countryCode){
