c9c0b1b6abceb4a2863c867e58adb1879286c509
Tejashegde95 <tejashegde.1595>
2022-02-20 11:30:53 +0530

    adding token in real time database

--- src/push-notification.js
+++ src/push-notification.js
@@ -7,7 +7,7 @@ export const initializeFirebase = () => {
         storageBucket: "news-delivery-service-dffe0.appspot.com",
         messagingSenderId: "573315736596",
         appId: "1:573315736596:web:8c89fc9461108587317301",
-        measurementId: "G-2F2B8PZ3K3"
+        measurementId: "G-2F2B8PZ3K3",
     });
   }
 
@@ -17,9 +17,21 @@ export const initializeFirebase = () => {
       await messaging.requestPermission();
       const token = await messaging.getToken();
       console.log('Your token is:', token);
-      alert(token);
+      var database = firebase.database().ref("/users")
+      database.push({
+        name : CreateGuid(),
+        token : token,
+      }).catch(alert);
       return token;
     } catch (error) {
       console.error(error);
     }
   }
+
+  function CreateGuid() {  
+    function _p8(s) {  
+       var p = (Math.random().toString(16)+"000000000").substr(2,8);  
+       return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;  
+    }  
+    return _p8() + _p8(true) + _p8(true) + _p8();  
+ }  
